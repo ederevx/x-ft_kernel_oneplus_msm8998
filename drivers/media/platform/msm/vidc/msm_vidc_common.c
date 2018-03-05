4a312b2be63e9b4e90f1b12032f49e56eb037f5b
Paras Nagda <pnagda@codeaurora.org>
2018-03-05 16:04:39 +0530

    msm: vidc: enable port reconfig for thumbnail session
    Send port reconfig to client for thumbnail session
    when driver received sufficient resources from firmware.
    
    This is needed as there are issues in DPB management
    in firmware.
    
    Change-Id: I7c2992e6be87fd277b209554d1aa7c4d0a088738
    Signed-off-by: Paras Nagda <pnagda@codeaurora.org>
    [Apply to k4.14]
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -1147,8 +1147,7 @@ static void handle_event_change(enum hal_command_response cmd, void *data)
 		rc = msm_comm_g_ctrl_for_id(inst,
 			V4L2_CID_MPEG_VIDC_VIDEO_CONTINUE_DATA_TRANSFER);
 
-		if (!is_thumbnail_session(inst) &&
-			(IS_ERR_VALUE((unsigned long)rc) || rc == false))
+		if ((IS_ERR_VALUE((unsigned long)rc) || rc == false))
 			event = V4L2_EVENT_SEQ_CHANGED_INSUFFICIENT;
 		else
 			event = V4L2_EVENT_SEQ_CHANGED_SUFFICIENT;
