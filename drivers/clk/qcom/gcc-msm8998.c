1b6d66f66ca2c3944bf17a9ce7d50a8c8095953c
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-09-02 05:37:37 -0400

    clk: qcom: gcc-msm8998: Remove gpll0_out_msscc parent
    Not defined in downstream and not needed as the enable reg is
    configured correctly.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/clk/qcom/gcc-msm8998.c
+++ drivers/clk/qcom/gcc-msm8998.c
@@ -1331,11 +1331,7 @@ static struct clk_gate2 gpll0_out_msscc = {
 		.enable_mask = BIT(2),
 		.hw.init = &(struct clk_init_data) {
 			.name = "gpll0_out_msscc",
-			.parent_names = (const char *[]){
-				"gpll0_ao",
-			},
-			.num_parents = 1,
-			.flags = CLK_ENABLE_HAND_OFF, // | CLK_SET_RATE_PARENT,
+			.flags = CLK_ENABLE_HAND_OFF,
 			.ops = &clk_gate2_ops,
 		},
 	},
