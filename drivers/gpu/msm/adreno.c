4ddb443d03e3a8f572dc0c9b14cfbaad4ab4f42e
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-09-04 16:36:06 -0400

    gpu: msm: adreno: Configure device_3d0 for A540
    The LLC booleans are never verified after 3d0 init which may cause
    userspace to think we support GPU LLC when in fact we don't. Same
    goes with the preempt struct, only A6XX supports these properties.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -110,13 +110,6 @@ static struct adreno_device device_3d0 = {
 	.profile.enabled = false,
 	.active_list = LIST_HEAD_INIT(device_3d0.active_list),
 	.active_list_lock = __SPIN_LOCK_UNLOCKED(device_3d0.active_list_lock),
-	.gpu_llc_slice_enable = true,
-	.gpuhtw_llc_slice_enable = true,
-	.preempt = {
-		.preempt_level = 1,
-		.skipsaverestore = 1,
-		.usesgmem = 1,
-	},
 };
 
 /* Ptr to array for the current set of fault detect registers */
