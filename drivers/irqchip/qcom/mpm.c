a01f4edad8599539a67f0abab26f86cb1569c47b
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-10-09 15:19:55 -0400

    irqchip: qcom: mpm: Use IRQF_NO_SUSPEND and pm_system_wakeup()
    MPM IRQ needs to be handled immediately during suspend path as
    it takes care of pending wakeup IRQs. Otherwise, there is a
    chance for a deadlock to happen when the MPM IRQ is only marked
    pending and suspended leaving the other important wakeup IRQs
    such as GPIOs to be left undetected as the IRQ is not able to
    mark these IRQs pending which would allow these IRQs to perform
    their needed wake events if present (such as the fingerprint
    sensor).
    
    Instead of using enable_irq_wake() which does not immediately wake
    the system at IRQ trigger and of which has also been advised
    against its usage with IRQF_NO_SUSPEND [1], use pm_system_wakeup()
    at the interrupt function instead. This is inline with the patch
    on qcom_glink_native given the fact the driver requires both to
    be set [2].
    
    This will ensure that the driver is able to operate at suspend
    while also being able to wakeup the system when needed, without
    going against the IRQ framework's restriction.
    
    [1]: https://github.com/ederevx/x-ft_kernel_oneplus_msm8998/blob/7438b633a6b073d66a3fa3678ec0dd5928caa4af/Documentation/power/suspend-and-interrupts.txt
    [2]: https://github.com/ederevx/x-ft_kernel_oneplus_msm8998/commit/8dad6a832262633adacba4edc137a920dead872e
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/irqchip/qcom/mpm.c
+++ drivers/irqchip/qcom/mpm.c
@@ -32,6 +32,7 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/cpu_pm.h>
+#include <linux/suspend.h>
 #include <asm/arch_timer.h>
 #include <soc/qcom/rpm-notifier.h>
 #include <soc/qcom/lpm_levels.h>
@@ -521,6 +522,9 @@ static irqreturn_t msm_mpm_irq(int irq, void *dev_id)
 	struct irq_desc *desc = NULL;
 	unsigned int reg = MPM_REG_ENABLE;
 
+	/* RPM hardware triggers this IRQ to wakeup system */
+	pm_system_wakeup();
+
 	for (i = 0; i < QCOM_MPM_REG_WIDTH; i++) {
 		value[i] = msm_mpm_read(reg, i);
 		trace_mpm_wakeup_enable_irqs(i, value[i]);
@@ -589,24 +593,15 @@ static int msm_mpm_init(struct device_node *node)
 	dev->ipc_irq = irq;
 
 	ret = request_irq(dev->ipc_irq, msm_mpm_irq,
-		IRQF_TRIGGER_RISING, "mpm",
+		IRQF_TRIGGER_RISING | IRQF_NO_SUSPEND, "mpm",
 		msm_mpm_irq);
 	if (ret) {
 		pr_err("request_irq failed errno: %d\n", ret);
 		goto ipc_irq_err;
 	}
 
-	ret = irq_set_irq_wake(dev->ipc_irq, 1);
-	if (ret) {
-		pr_err("failed to set wakeup irq %lu: %d\n",
-			dev->ipc_irq, ret);
-		goto set_wake_irq_err;
-	}
-
 	return register_system_pm_ops(&pm_ops);
 
-set_wake_irq_err:
-	free_irq(dev->ipc_irq, msm_mpm_irq);
 ipc_irq_err:
 	iounmap(dev->mpm_ipc_reg);
 ipc_reg_err:
