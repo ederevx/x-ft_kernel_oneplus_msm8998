e90aa1ee9c7c2a0d2d7c45cfc2d5855960f6ac68
Edrick Sinsuan <evcsinsuan@gmail.com>
2025-07-30 18:10:43 -0400

    sched: ucassist: Introduce task-specific UCLAMP assist
    Let's further extend UCLAMP assist to be able to configure
    task-specific UCLAMP configurations by targetting their task->comm
    name. This will allow us to specifically target important system and
    kernel processes and define taskgroup-persistent UCLAMP values to
    further reduce jitter without increasing system load significantly
    as we only prioritize these defined targets.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- kernel/sched/ucassist.c
+++ kernel/sched/ucassist.c
@@ -2,16 +2,19 @@
 /*
  * kernel/sched/ucassist.c
  *
- * Copyright (C) 2024, Edrick Vince Sinsuan
+ * Copyright (C) 2024-2025, Edrick Vince Sinsuan
  *
- * This provides the kernel a way to configure uclamp
- * values at init.
+ * This provides the kernel a way to configure uclamp values at init
+ * for taskgroups and during runtime for defined tasks.
  */
 #define pr_fmt(fmt) "ucassist: %s: " fmt, __func__
 
 #include <linux/sched.h>
 
 #include "sched.h"
+
+#define SCHED_CAPACITY_SCALE_PERC(perc) \
+		((perc * SCHED_CAPACITY_SCALE) / 100)
 
 int cpu_uclamp_write_css(struct cgroup_subsys_state *css, char *buf,
 					enum uclamp_id clamp_id);
@@ -27,13 +30,19 @@ struct uclamp_data {
 	bool boosted;
 };
 
-struct ucassist_struct {
+struct ucassist_css_struct {
 	const char *name;
 	struct uclamp_data data;
 	bool initialized;
 };
 
-static struct ucassist_struct ucassist_data[] = {
+struct ucassist_task_struct {
+	const char target[TASK_COMM_LEN];
+	unsigned int uclamp_max;
+	unsigned int uclamp_min;
+};
+
+static const struct ucassist_css_struct ucassist_css_data[] = {
 	{
 		.name = "top-app",
 		.data = { "max", "1", 1, 1 },
@@ -64,7 +73,35 @@ static struct ucassist_struct ucassist_data[] = {
 	},
 };
 
-static void ucassist_set_uclamp_data(struct cgroup_subsys_state *css,
+static const struct ucassist_task_struct ucassist_task_data[] = {
+	{
+		.target = "kgsl_worker",
+		.uclamp_max = SCHED_CAPACITY_SCALE, 
+		.uclamp_min = SCHED_CAPACITY_SCALE_PERC(30),
+	},
+	{
+		.target = "mdss_fb",
+		.uclamp_max = SCHED_CAPACITY_SCALE, 
+		.uclamp_min = SCHED_CAPACITY_SCALE_PERC(40),
+	},
+	{
+		.target = "surfaceflinger",
+		.uclamp_max = SCHED_CAPACITY_SCALE, 
+		.uclamp_min = SCHED_CAPACITY_SCALE_PERC(30),
+	},
+	{
+		.target = "system_server",
+		.uclamp_max = SCHED_CAPACITY_SCALE, 
+		.uclamp_min = SCHED_CAPACITY_SCALE_PERC(10),
+	},
+	{
+		.target = "vsync_retire",
+		.uclamp_max = SCHED_CAPACITY_SCALE, 
+		.uclamp_min = SCHED_CAPACITY_SCALE_PERC(20),
+	},
+};
+
+static void ucassist_set_css_uclamp_data(struct cgroup_subsys_state *css,
 		struct uclamp_data cdata)
 {
 	cpu_uclamp_write_css(css, cdata.uclamp_max, 
@@ -80,26 +117,46 @@ static void ucassist_set_uclamp_data(struct cgroup_subsys_state *css,
 
 int cpu_ucassist_init_values(struct cgroup_subsys_state *css)
 {
-	struct ucassist_struct *uc;
+	const struct ucassist_css_struct *uc;
 	int i;
 
 	if (!css->cgroup->kn)
 		return -EINVAL;
 
-	for (i = 0; i < ARRAY_SIZE(ucassist_data); i++) {
-		uc = &ucassist_data[i];
-
-		if (uc->initialized)
-			continue;
+	for (i = 0; i < ARRAY_SIZE(ucassist_css_data); i++) {
+		uc = &ucassist_css_data[i];
 
 		if (strcmp(css->cgroup->kn->name, uc->name))
 			continue;
 
 		pr_info("setting values for %s", uc->name);
-		ucassist_set_uclamp_data(css, uc->data);
-
-		uc->initialized = true;
+		ucassist_set_css_uclamp_data(css, uc->data);
+		break;
 	}
 
 	return 0;
+}
+
+int task_ucassist_get_uclamp_data(struct task_struct *p, 
+				unsigned int *min, unsigned int *max)
+{
+	const struct ucassist_task_struct *uc;
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(ucassist_task_data); i++) {
+		uc = &ucassist_task_data[i];
+
+		if (likely(!strstr(p->comm, uc->target)))
+			continue;
+
+		if (*min == uc->uclamp_min && *max == uc->uclamp_max)
+			return -EEXIST;
+
+		pr_info("setting values for %s", p->comm);
+		*min = uc->uclamp_min;
+		*max = uc->uclamp_max;
+		return 0;
+	}
+
+	return -EINVAL;
 }
