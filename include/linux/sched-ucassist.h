f1c480e4fe6f668f012bc03f13004320921c5f6d
Edrick Sinsuan <evcsinsuan@gmail.com>
2025-08-12 16:40:29 -0400

    sched: ucassist: Fix setscheduler(_task)_ucassist hooks
    There is no need to hold the task RQ lock while we're just checking
    the task->comm string and reading the UCLAMP values. Create two
    different paths accomodating whether the task RQ lock has already
    been acquired or not; get the task->comm string and evaluate the
    task UCLAMP accordingly and reduce the amount of time spent by
    non-scheduler callers holding its own task RQ lock which can freeze
    up other tasks within the same RQ.
    
    Also fix __setscheduler_ucassist UCLAMP override and enforce values
    regardless of scheduler policy and add slow-path mutex lock in the
    sched uclamp ucassist helper function.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- include/linux/sched-ucassist.h
+++ include/linux/sched-ucassist.h
@@ -11,9 +11,32 @@ extern bool ucassist_restrict_enabled;
 
 #ifdef CONFIG_UCLAMP_TASK
 
-int ucassist_get_task_uclamp_data(struct task_struct *p, 
-				unsigned int *min, unsigned int *max, 
+int __ucassist_get_task_uclamp_data(const char *comm, 
+				unsigned int *min, unsigned int *max,
 				unsigned int flags);
+
+static inline int ucassist_get_task_uclamp_data(struct task_struct *p, 
+				unsigned int *min, unsigned int *max, 
+				unsigned int flags)
+{
+	char comm[TASK_COMM_LEN];
+	get_task_comm(comm, p);
+	return __ucassist_get_task_uclamp_data(comm, min, max, flags);
+}
+
+static inline bool __ucassist_task_is_target(const char *comm,
+				unsigned int flags)
+{
+	unsigned int min, max;
+	return !__ucassist_get_task_uclamp_data(comm, &min, &max, flags);
+}
+
+static inline bool ucassist_task_is_target(struct task_struct *p,
+				unsigned int flags)
+{
+	unsigned int min, max;
+	return !ucassist_get_task_uclamp_data(p, &min, &max, flags);
+}
 
 void ucassist_sched_uclamp_set(unsigned int min, unsigned int max);
 
@@ -27,14 +50,41 @@ int ucassist_init_cpu_values(struct cgroup_subsys_state *css) { }
 
 #else /* CONFIG_UCLAMP_TASK */
 
+static inline int __ucassist_get_task_uclamp_data(const char *comm, 
+				unsigned int *min, unsigned int *max,
+				unsigned int flags) 
+{ 
+	return 0;
+}
+
 static inline int ucassist_get_task_uclamp_data(struct task_struct *p, 
 				unsigned int *min, unsigned int *max,
-				unsigned int flags) { }
+				unsigned int flags) 
+{
+	return 0;
+}
 
-static inline void ucassist_sched_uclamp_set(struct task_struct *p) { }
+static inline bool __ucassist_task_is_target(const char *comm,
+				unsigned int flags)
+{
+	return false;
+}
+
+static inline bool ucassist_task_is_target(struct task_struct *p,
+				unsigned int flags)
+{
+	return false;
+}
+
+static inline void ucassist_sched_uclamp_set(struct task_struct *p) 
+{	
+}
 
 static inline int setscheduler_task_ucassist(struct task_struct *p,
-				unsigned int flags) { }
+				unsigned int flags) 
+{
+	return 0;
+}
 
 #endif /* CONFIG_UCLAMP_TASK */
 
